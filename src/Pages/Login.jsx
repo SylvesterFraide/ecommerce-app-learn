5a810093c9adebe034d7992dc8e8bb838d044782
SylvesterFraide <fridaysylvester2@gmail.com>
2025-10-25 21:40:16 +0300

    32nd commit

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,4 +1,23 @@
 import React, { useState } from 'react'
 
+const Login = () => {
+  const [currentState, setCurrentState] = useState('Sign Up');
+
+  return (
+    <form className="flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4 text-gray-800">
+      <div className="inline-flex items-center gap-2 mb-2 mt-10">
+        <p className='text-3xl prata-regular'>{currentState}</p>
+      </div>
+     {currentState === 'Login' ? '' :  <input type="text" className="w-full px-3 py-2 border border-gray-800" placeholder='Name' required />}
+      <input type="email" className="w-full px-3 py-2 border border-gray-800" placeholder='Email' required />
+      <input type="password" className="w-full px-3 py-2 border border-gray-800" placeholder='Password' required />
+      <div className="w-full flex justify-between text-sm mt-[-8px]">
+        <p className="">Forgot your password</p>
+        {currentState === 'Login' ? <p>Create account</p> : <p>Login here</p>}
+        
+      </div>
+    </form>
+  )
+}
 
 export default Login;
