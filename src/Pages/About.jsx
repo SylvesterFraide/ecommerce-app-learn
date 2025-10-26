1237a9cd594b7750a0c91f29d0f7aa46757821d8
SylvesterFraide <fridaysylvester2@gmail.com>
2025-10-26 13:31:35 +0300

    33rd commit

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,9 +1,71 @@
-import React from 'react'
+import React from "react";
+import Title from "../Components/Title";
+import AboutImg from "../assets/about/About.jpg";
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <div>
+      <div className="text-2xl text-center pt-8 border-t">
+        <Title text1={"ABOUT"} text2={"US"} />
+      </div>
+      <div className="my-10 flex flex-col md:flex-row gap-16">
+        <img src={AboutImg} alt="" className="w-full md:max-w-[450px]" />
+        <div className="flex flex-col justify-center gap-6 md:w-2/4 text-gray-400">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum quos
+            reiciendis accusamus odio, nostrum minima, distinctio excepturi ut
+            deleniti perferendis fugiat dolorem magnam assumenda dolorum. Cum
+            aut officiis deserunt fugiat.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi
+            facilis ullam tempora itaque, quaerat eaque porro voluptate maiores
+            temporibus repellendus minima magnam. Porro, expedita aliquid.
+            Deserunt impedit consequuntur ex iure.
+          </p>
+          <b className="text-gray-800">Our Mission</b>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae,
+            eveniet voluptatem illum voluptas repellat tenetur consequuntur aut
+            unde amet quis iusto saepe eligendi repudiandae adipisci velit ut
+            minus rem assumenda?
+          </p>
+        </div>
+      </div>
+      <div className="text-4xl py-4">
+        <Title text1={"WHY"} text2={"CHOOSE US."} />
+      </div>
+      <div className="flex flex-col md:flex-row text-sm mb-28">
+        <div className="bg-gray-100 px-10 md:px-16 py-8 sm:py-10 flex flex-col gap-5 ">
+          <b className="">Quality Assurance:</b>
+          <p className="text-gray-600" >
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Est
+            eligendi pariatur, veniam a soluta dolores ullam enim labore, amet
+            numquam, culpa accusamus recusandae ut excepturi aliquam velit
+            exercitationem id magni!
+          </p>
+        </div>
+        <div className="bg-gray-100 px-10 md:px-16 py-8 sm:py-10 flex flex-col gap-5">
+          <b className="">Convenience:</b>
+          <p className="text-gray-600">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Est
+            eligendi pariatur, veniam a soluta dolores ullam enim labore, amet
+            numquam, culpa accusamus recusandae ut excepturi aliquam velit
+            exercitationem id magni!
+          </p>
+        </div>
+        <div className="bg-gray-100 px-10 md:px-16 py-8 sm:py-10 flex flex-col gap-5">
+          <b className="">Exceptional Customer Service:</b>
+          <p className="text-gray-600">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Est
+            eligendi pariatur, veniam a soluta dolores ullam enim labore, amet
+            numquam, culpa accusamus recusandae ut excepturi aliquam velit
+            exercitationem id magni!
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default About;
--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,23 +1,58 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 const Login = () => {
-  const [currentState, setCurrentState] = useState('Sign Up');
+  const [currentState, setCurrentState] = useState("Sign Up");
 
   return (
-    <form className="flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4 text-gray-800">
+    <form onSubmit={(e) => e.preventDefault()} className="flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4 text-gray-800">
       <div className="inline-flex items-center gap-2 mb-2 mt-10">
-        <p className='text-3xl prata-regular'>{currentState}</p>
+        <p className="text-3xl prata-regular">{currentState}</p>
       </div>
-     {currentState === 'Login' ? '' :  <input type="text" className="w-full px-3 py-2 border border-gray-800" placeholder='Name' required />}
-      <input type="email" className="w-full px-3 py-2 border border-gray-800" placeholder='Email' required />
-      <input type="password" className="w-full px-3 py-2 border border-gray-800" placeholder='Password' required />
+      {currentState === "Login" ? (
+        ""
+      ) : (
+        <input
+          type="text"
+          className="w-full px-3 py-2 border border-gray-800"
+          placeholder="Name"
+          required
+        />
+      )}
+      <input
+        type="email"
+        className="w-full px-3 py-2 border border-gray-800"
+        placeholder="Email"
+        required
+      />
+      <input
+        type="password"
+        className="w-full px-3 py-2 border border-gray-800"
+        placeholder="Password"
+        required
+      />
       <div className="w-full flex justify-between text-sm mt-[-8px]">
-        <p className="">Forgot your password</p>
-        {currentState === 'Login' ? <p>Create account</p> : <p>Login here</p>}
-        
+        <p className="cursor-pointer">Forgot your password</p>
+        {currentState === "Login" ? (
+          <p
+            onClick={() => setCurrentState("Sign Up")}
+            className="cursor-pointer"
+          >
+            Create account
+          </p>
+        ) : (
+          <p
+            onClick={() => setCurrentState("Login")}
+            className="cursor-pointer"
+          >
+            Login here
+          </p>
+        )}
       </div>
+      <button className=" bg-black px-8 py-2 text-white font-light mt-4 rounded">
+        {currentState === "Login" ? "Sign In" : "Sign Up"}
+      </button>
     </form>
-  )
-}
+  );
+};
 
 export default Login;
